7a67ed5e56eb31e841d2d2917842624ac6df5fbf
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-06-27 14:45:41 +0300

    initial commit - convert to kotlin

---
+++ src/main/java/me/olios/plugins/randomutils/RandomUtils.kt
@@ -0,0 +1,13 @@
+package me.olios.plugins.randomutils
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RandomUtils : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
